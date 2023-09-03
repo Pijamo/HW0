9492c4a6d56d498c218c6cce2977ef0a57f402a7
Pijamo <ngulliejake@gmail.com>
2023-09-04 01:48:47 +0530

    Initial commit

---
+++ src/cis5550/HelloWorld.java
@@ -0,0 +1,8 @@
+package cis5550;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name = "Pijamo Ngullie";
+        System.out.println("Hello World (" + name + ")");
+    }
+}
